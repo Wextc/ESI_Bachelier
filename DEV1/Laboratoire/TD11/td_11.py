29b4693c00fcf96151f9c22b5d1e7284abbd407f
Wextc <wextc76@gmail.com>
2025-12-15 00:21:42 +0100

    DEV

--- DEV1/Laboratoire/TD11/td_11.py
+++ DEV1/Laboratoire/TD11/td_11.py
@@ -2,4 +2,23 @@ liste1 = [1,2,3]
 print(len(liste1))
 print(0 in liste1)
 liste1.append(15)
+liste1.reverse()
 print(liste1)
+# Affiche un élément choisi dont on connait l'indice de départ et de fin
+liste1 = [1,2,3]
+liste4 = liste1[2:3]
+print("Affiche l'élément qui commence à l'index 2 et se termine à l'index 3 :",liste1[2:3])
+
+# Compte la récurrence de l'élément dans count(...)
+liste5 = [15,15,15, 3, 2, 1]
+print("Compte la récurrence de l'élément 15: ",liste5.count(15))
+print("Compte la récurrence de l'élément 3: ",liste5.count(3)) # liste6 = liste5.count(3)  print(liste6) :> donne même résultat.
+
+#Insert. On prend l'élément d'index 1 puis avant lui  on insert 7.
+liste6 = liste5.insert(1,7)
+print("Insertion :",liste5)
+
+# Modification. On prend l'élément d'index 1 et on le remplace par 7
+liste5 = [15,15,15, 3, 2, 1]
+liste5[1] = 7
+print("Modification :",liste5)
