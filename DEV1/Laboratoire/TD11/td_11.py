12f73d708b83bcbad06d19c8750b2305d39061fe
Wextc <wextc76@gmail.com>
2025-12-15 02:27:39 +0100

    DEV

--- DEV1/Laboratoire/TD11/td_11.py
+++ DEV1/Laboratoire/TD11/td_11.py
@@ -36,14 +36,79 @@ liste1 = [1, 2, 3, 5, 6, 7, 8, 9]
 liste2 = ["a", "b", "c", "d", "e", "f", "g", "h"]
 liste3 = liste1.copy() 
 print("Liste copiée :", liste3)  # [1, 2, 3, 5, 6, 7, 8, 9]
+
 # Si ton objectif est de combiner liste1 et liste2
 liste3 =  liste2 + liste1
 print("Liste1 et liste2 sont concotenaté :", liste3)
+
 # Extraire valeur
 liste3 = [1, 2, 3, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h"]
 liste1 = liste3[8:]
 print("La valeur de liste1 :", liste1)
 
+# extrayez de la liste1 l’élément en 4ème position ;
+
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+print("Elément à l'indice 4 position : ", liste1[3])
+
+# triez liste1 en ordre ascendant (du plus petit au plus grand) ;
+
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+liste1.sort()
+print("Liste1 triée par ordre ascendant :", liste1)
+
+
+# triez liste2 en ordre descendant (du plus grand au plus petit) ;
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+liste2.sort(reverse=True)
+print("Liste inversée :", liste2)
+
+
+# cherchez l’indice d’une valeur présente dans liste2 ;
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+print("La valeur de l'élément avec l'indice 7 : ",liste2.index(7))
+
+
+
+# recherchez l’indice d’une valeur non présente dans liste2 ;
+
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+# liste2.index(100) # ValueError: 100 is not in list. Impossible
+
+
+#supprimez une valeur présente dans liste2 ;
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+liste2.remove(6)
+print("Liste2 sans l'élément de valeur 6:", liste2) # résutat :  [9, 8, 7, 5, 3, 2, 1]
+
+
+# que se passe-t-il si vous supprimez une valeur non présente dans liste2 ?
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+#liste2.remove(100) # ValueError: list.remove(x): x not in list 
+
+ 
+# Ajoute à la fin de liste1 les éléments de liste2 situés entre la 3ième et la 7ième position inclues ;
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+liste1.extend(liste2[2:7])
+print("Insertion des éléments entre l'indice 2 et 7 de liste2 qui étend liste1 :",liste1)
+
+
+# Supprime toutes les valeurs de liste2 ;
+liste2 = [1, 2, 3, 5, 6, 7, 8, 9]
+liste2.clear()
+print("Liste vidée :", liste2) # Résultal :   []
+
+
+
+# Supprimer le premier élément
+liste1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+del liste1[0]
+print("Liste sans le premier élémment ici 'a': ", liste1)
 
 
 
