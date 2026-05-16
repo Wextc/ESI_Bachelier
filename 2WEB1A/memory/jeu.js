5f100ed4063f597fdee529aa2e96b05e77b43d0c
68600 <68600@etu.he2b.be>
2026-05-16 15:14:51 +0200

    WEB1

--- 2WEB1A/memory/jeu.js
+++ 2WEB1A/memory/jeu.js
@@ -5,24 +5,33 @@ const Game = {
   step: "",
 };
 
-const startGame = () => {
-  btnState();
-  seqState();
-};
-
 const btnState = () => {
   document.getElementById("btn").classList.add("hidden");
 };
 
-const seqState = () => {
+const seqStates = () => {
   let arrayNbr = [1, 2, 3, 4];
 
-  let seq = document.getElementById("seq");
-  arrayNbr.forEach((item) => {
+  var seq = document.getElementById("seq");
+  arrayNbr.forEach((item, index) => {
     let span = document.createElement("span");
+    span.setAttribute("id", index);
     span.textContent = item;
     seq.append(span);
   });
+};
+
+const noNbr = () => {
+  const spans = seq.querySelectorAll("span");
+  spans.forEach((el, index) => {
+    el.textContent = "X";
+  });
+};
+
+const startGame = () => {
+  btnState();
+  seqStates();
+  setTimeout(noNbr, 3000);
 };
 
 document.getElementById("btn").addEventListener("click", startGame);
